6474cd302c52c467446b6f8f6baed51cfc7de159
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-01-15 19:25:52 +0300

    feat: add interactions with server side

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,33 +1,81 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
-import cn from 'classnames';
+import _ from 'lodash';
 
-function App() {
-  const [message, setMessage] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
+function Card(props) {
+  const { id, name, customName, image } = props.value;
+  return (
+    <div className="card">
+      <div className="card-img-container">
+        <img src={image} className="card-img-top" alt={name}/>
+      </div>
+      <div className="card-body">
+        <h4 className="card-title">{name}</h4>
+        <p className="card-text">
+          <b>name: </b>
+          {customName}
+        </p>
+        <p className="card-text">
+          <b>state: </b>
+          <span className="green">washing</span>
+        </p>
+        <button className="btn btn-primary" onClick={() => props.toggleToAppliance(id)}>Manage</button>
+      </div>
+    </div>
+  );
+}
+
+function Appliance(props) {
+  const { image, name, customName, info } = props.value;
+  return (
+    <div className="container appliance">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <div className="appliance-img-container">
+            <img src={image} className="appliance-img" alt={name}/>
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <h2 className="title">{name}</h2>
+          <p className="text">
+            <b>name: </b>
+            {customName}
+          </p>
+          <p className="text">
+            <b>state: </b>
+            <span className="green">washing</span>
+          </p>
+          {Object.entries(info).map(([key, value]) => (
+            <div key={_.uniqueId()}>
+              <h4>{key}</h4>
+              <ul>
+                {Array.isArray(value)
+                  ? value.map((v) => <li>{v}</li>)
+                  : Object.entries(value).map(([k, v]) => <li><strong>{k}</strong> {v}</li>)}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
   const [url, setUrl] = useState('/api');
+  const [cards, setCards] = useState(null);
+  const [applianceId, toggleToAppliance] = useState(null);
 
-  const fetchData = useCallback(() => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setMessage(json.message);
-        setIsFetching(false);
-      }).catch(e => {
-        setMessage(`API call failed: ${e}`);
-        setIsFetching(false);
-      })
-  }, [url]);
+  const getData = async () => {
+    const newParams = await axios.get(url, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    setCards(newParams.data);
+  };
 
   useEffect(() => {
-    setIsFetching(true);
-    fetchData();
-  }, [fetchData]);
+    if (!cards) getData();
+  });
 
   return (
     <div className="App">
@@ -37,22 +85,44 @@ function App() {
           <h1>SmartAppliances</h1>
         </a>
       </header>
-      <div className="card-group">
-        <div className="card">
-          <div className="card-img-container">
-            <img src="./__fixtures__/images/wash_machine_EW8F1R48B_1.jpg" className="card-img-top" alt="wash machine EW8F1R48B"/>
-          </div>
-          <div className="card-body">
-            <h4 className="card-title">Wash Machine</h4>
-            <p className="card-text"><b>name:</b> in bathroom</p>
-            <p className="card-text"><b>state:</b> <span className="state">washing</span></p>
-            <a href="#" className="btn btn-primary">Manage</a>
-          </div>
-        </div>
-      </div>
+      {applianceId ? (
+        <>
+          <nav className="breadcrumb-main" aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <button type="button" class="btn btn-link my-btn-home" onClick={() => toggleToAppliance(null)}>
+                  My Smart Appliances
+                </button>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                {cards.find((card) => card.id === applianceId).name}
+              </li>
+            </ol>
+          </nav>
+          <main className="appliance">
+            <Appliance value={cards.find((card) => card.id === applianceId)} />
+          </main>
+        </>
+      ) : (
+        <>
+          <nav className="breadcrumb-main" aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item active" aria-current="page">
+                My Smart Appliances
+              </li>
+            </ol>
+          </nav>
+          <main className="cards">
+            {cards && cards.map((card) => <Card  key={card.id} value={card} toggleToAppliance={toggleToAppliance} />)}
+            <div className="card">
+              <div className="card-body">
+                <p className="card-text plus">+</p>
+                <a href="#" className="btn btn-primary flex">Add new appliance</a>
+              </div>
+            </div>
+          </main>
+        </>
+      )}
     </div>
   );
-
 }
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -1,42 +1,59 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const _ = require('lodash');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
+const getMyAppliances = (absoluteFileName) => {
+  const myAppliances = fs.readFileSync(absoluteFileName, 'utf-8');
+  return JSON.parse(myAppliances);
+};
+
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
+  // eslint-disable-next-line no-console
   console.error(`Node cluster master ${process.pid} is running`);
 
   // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < numCPUs; i += 1) {
     cluster.fork();
   }
 
   cluster.on('exit', (worker, code, signal) => {
+    // eslint-disable-next-line no-console
     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
   });
-
 } else {
   const app = express();
 
   // Priority serve any static files.
+  app.use('/static', express.static(path.resolve(__dirname, '../public')));
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
+  app.use(express.json()); // for parsing application/json
+  app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+  
+  // Get absolute name of json keeping data of users appliances
+  const absoluteFileName = path.resolve(__dirname, '../app-data/my-appliances.json');
+
   // Answer API requests.
-  app.get('/api', function (req, res) {
+  app.get('/api', (req, res) => {
+    const myAppliances = getMyAppliances(absoluteFileName);
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.json(myAppliances);
   });
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
+  app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.error(`Node ${isDev ? 'dev server' : `cluster worker ${process.pid}`}: listening on port ${PORT}`);
   });
 }
