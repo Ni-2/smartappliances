31a1ddee30e7caf6ceacff12f2500f361bc0dc7f
Nikita Teremovskiy <teremovskiin@gmail.com>
2021-01-19 16:05:16 +0300

    fix: unset task in the form; fixed saving of the wrong statuses.

--- server/sendTaskToDevice.js
+++ server/sendTaskToDevice.js
@@ -2,22 +2,16 @@ import fs  from 'fs';
 import path from 'path';
 
 export default (id) => {
-    const allSerialsFileName = path.resolve(__dirname, '../app-data/all-serials.json');
+  const allSerialsFileName = path.resolve(__dirname, '../app-data/all-serials.json');
+  const changeTask = (task) => {
     const allSerialsJson = fs.readFileSync(allSerialsFileName, 'utf-8');
     const allSerials = JSON.parse(allSerialsJson);
-
-    setTimeout(() => {
-        allSerials[id].status = 'Done!';
-        fs.writeFileSync(allSerialsFileName, JSON.stringify(allSerials, null, 4), 'utf-8');
-    }, 15000);
-
-    setTimeout(() => {
-        allSerials[id].status = 'available';
-        fs.writeFileSync(allSerialsFileName, JSON.stringify(allSerials, null, 4), 'utf-8');
-    }, 30000);
-
-    allSerials[id].status = 'in operation';
+    allSerials[id].status = task;
     fs.writeFileSync(allSerialsFileName, JSON.stringify(allSerials, null, 4), 'utf-8');
+  };
 
-    return true;
+  setTimeout((task) => changeTask(task), 15000, 'Done!');
+  setTimeout((task) => changeTask(task), 30000, 'available');
+  changeTask('in operation');
+  return true;
 };
